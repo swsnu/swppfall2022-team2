fb6c7c11fb40a7fd82399eba20d8a4ea167c42cf
0114kek <0114kek@snu.ac.kr>
2022-10-21 20:42:44 +0900

    MatchingStatus prototype

--- frontend/src/matching/MatchingStatus.tsx
+++ frontend/src/matching/MatchingStatus.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const MatchingStatus = () => {
-  return <div>MatchingStatus</div>;
+  const [matched, handleMatched] = useState(false);
+  //for check whether matching is done, maybe should be changed to use Redux
+  return matched ? (
+    <div>
+      <p>Matching Status</p>
+      <div>
+        information about the succeed matching
+        <div>ex:profile image, gender, age</div>
+      </div>
+      <div>Num of people matching now</div>
+    </div>
+  ) : (
+    <div>
+      <p>Matching Status</p>
+      <div>not matched yet</div>
+      <div>Num of people matching now</div>
+    </div>
+  );
 };
 
 export default MatchingStatus;

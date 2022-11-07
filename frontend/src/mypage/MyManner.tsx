04669f551d1e6a6ba80de96c4e9354f5f4dc4028
mkson417 <tommy417@naver.com>
2022-11-07 16:38:49 +0900

    bar

--- frontend/src/mypage/MyManner.tsx
+++ frontend/src/mypage/MyManner.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import './MyPage.css';
 
 const MyManner: React.FC = () => {
+    function TempBar(){
+        const now = 36.5
+        return <ProgressBar now = {now}  className='tempbar'/>;
+    }
     return (
         <div>
-            <p>
-                적당한 이미지
-            </p>
+            <div className = 'barcontainer'>
+                <TempBar/>
+            </div>
             <p>
                 당신의 매너 온도는 36.5입니다.
             </p>

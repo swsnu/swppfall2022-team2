d7a967cf39ba1a49bdcf0e26b2057d2cc88f3c79
byoungwookan97 <byoungwookan@snu.ac.kr>
2022-11-08 19:21:56 +0900

    Menu fixed

--- frontend/src/menus/Menu.tsx
+++ frontend/src/menus/Menu.tsx
@@ -1,4 +1,6 @@
-import { Card } from 'react-bootstrap';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 
@@ -8,24 +10,52 @@ const Menu = () => {
         <div className="card menu-list overflow-auto">
             <div className="card-header">
                 <h5 className="card-title">Today's Menu</h5>
-                <ul className="nav nav-tabs card-header-tabs" data-bs-tabs="tabs">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="true" data-bs-toggle="tab" href="#dhcp">DHCP</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" data-bs-toggle="tab" href="#static">Static</a>
-                    </li>
-                </ul>
             </div>
-            <form className="card-body tab-content">
-                <div className="tab-pane active" id="dhcp">
-                    <p className="card-text">Change DHCP Network settings.</p>
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+                <Tab eventKey="home" title="아침">
+                <div className="row">
+                    <div className="col-sm-6">
+                            <Card border="primary">
+                                <Card.Header>학생회관</Card.Header>
+                                <Card.Body>                 
+                                    <Card.Text>
+                                        제육볶음    3000
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                    </div>
+                    <div className="col-sm-6">
+                        <Card border="secondary" style={{ width: '17rem' }}>
+                            <Card.Header>아워홈</Card.Header>
+                            <Card.Body>                 
+                                <Card.Text>
+                                    야채탕&청포묵김가루무침    3000<br/>
+                                    베이글&마늘빵&슬라이스햄&치즈   4000
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
                 </div>
-                <div className="tab-pane" id="static">
-                    <p className=" card-text">Change Static Network settings.</p>
-                </div>
-                <button className="btn btn-primary" type="submit">Save</button>
-            </form>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <Card border="success" style={{ width: '17rem' }}>
+                            <Card.Header>기숙사식당</Card.Header>
+                            <Card.Body>                 
+                                <Card.Text>
+                                    제육숙주볶음    4500
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>                       
+                </Tab>
+                <Tab eventKey="profile" title="점심">
+                   
+                </Tab>
+                <Tab eventKey="contact" title="저녁">
+                    
+                </Tab>
+            </Tabs>
        </div>
     )
 }

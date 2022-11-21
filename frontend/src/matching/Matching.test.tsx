a1fcc8c6a08125ca05fd6a8081732997ab7b8827
0114kek <0114kek@snu.ac.kr>
2022-11-21 18:31:40 +0900

    matching frontend test

--- frontend/src/matching/Matching.test.tsx
+++ frontend/src/matching/Matching.test.tsx
@@ -154,9 +154,10 @@ describe('Matching', () => {
       } else return Promise.reject();
     });
     const matchingRender = render(matching);
-    await waitFor(() => screen.getByText('Check Matching'));
-    const checkButton = matchingRender.container.querySelector('#checkButton');
-    fireEvent.click(checkButton!);
+    await waitFor(() => screen.getByText('4명의 사람들이 현재 매칭 중입니다'));
+    await waitFor(() => screen.getByText('4명의 사람들이 현재 매칭 중입니다'));
+    //const checkButton = matchingRender.container.querySelector('#checkButton');
+    //fireEvent.click(checkButton!);
   });
   it('check matching when matchingId=3', async () => {
     jest.spyOn(axios, 'get').mockImplementation((url) => {
@@ -186,9 +187,130 @@ describe('Matching', () => {
       } else return Promise.reject();
     });
     const matchingRender = render(matching);
-    await waitFor(() => screen.getByText('Check Matching'));
-    const checkButton = matchingRender.container.querySelector('#checkButton');
-    fireEvent.click(checkButton!);
+    await waitFor(() => screen.getByText('4명의 사람들이 현재 매칭 중입니다'));
+    await waitFor(() => screen.getByText('매칭이 완료되었습니다'));
+  });
+  it('stop matching', async () => {
+    jest.spyOn(axios, 'get').mockImplementation((url) => {
+      if (url === `matching/check/3/`) {
+        return Promise.resolve({
+          status: 201,
+          data: {
+            time: '0',
+            space_user: '',
+            spaceOpponent: '',
+            mbti: 'ESTJ',
+            gender: 'M',
+            age: '22',
+            id: '1',
+            last_name: 'a',
+            first_name: 'b',
+          },
+        });
+      } else if (url === `matching/get`) {
+        return Promise.resolve({
+          status: 201,
+          data: {
+            id: 3,
+            num_matching: 4,
+          },
+        });
+      } else return Promise.reject();
+    });
+    jest.spyOn(axios, 'delete').mockImplementation((url) => {
+      if (url === `matching/stop/`) {
+        return Promise.resolve({
+          status: 200,
+          data: {},
+        });
+      } else return Promise.reject();
+    });
+    const matchingRender = render(matching);
+    await waitFor(() => screen.getByText('4명의 사람들이 현재 매칭 중입니다'));
+    const stopButton = matchingRender.container.querySelector('#stopButton');
+    fireEvent.click(stopButton!);
+    await waitFor(() => expect(axios.delete).toHaveBeenCalled());
+  });
+  it('stop matching when matched', async () => {
+    jest.spyOn(axios, 'get').mockImplementation((url) => {
+      if (url === `matching/check/3/`) {
+        return Promise.resolve({
+          status: 201,
+          data: {
+            time: '0',
+            space_user: '',
+            spaceOpponent: '',
+            mbti: 'ESTJ',
+            gender: 'M',
+            age: '22',
+            id: '1',
+            last_name: 'a',
+            first_name: 'b',
+          },
+        });
+      } else if (url === `matching/get`) {
+        return Promise.resolve({
+          status: 201,
+          data: {
+            id: 3,
+            num_matching: 4,
+          },
+        });
+      } else return Promise.reject();
+    });
+    jest.spyOn(axios, 'delete').mockImplementation(() => {
+      return Promise.reject();
+    });
+    const matchingRender = render(matching);
+    await waitFor(() => screen.getByText('4명의 사람들이 현재 매칭 중입니다'));
+    const stopButton = matchingRender.container.querySelector('#stopButton');
+    fireEvent.click(stopButton!);
+    await waitFor(() => expect(axios.delete).toHaveBeenCalled());
+    //await waitFor(() => screen.getByText('매칭이 완료되었습니다'));
+  });
+  it('end matching', async () => {
+    jest.spyOn(axios, 'get').mockImplementation((url) => {
+      if (url === `matching/check/3/`) {
+        return Promise.resolve({
+          data: {
+            time: '0',
+            space_user: '',
+            spaceOpponent: '',
+            mbti: 'ESTJ',
+            gender: 'M',
+            age: '22',
+            id: '1',
+            last_name: 'a',
+            first_name: 'b',
+          },
+        });
+      } else if (url === `matching/get`) {
+        return Promise.resolve({
+          status: 200,
+          data: {
+            time: '0',
+            space_user: '',
+            spaceOpponent: '',
+            mbti: 'ESTJ',
+            gender: 'M',
+            age: '22',
+            id: '1',
+            last_name: 'a',
+            first_name: 'b',
+          },
+        });
+      } else return Promise.reject();
+    });
+
+    jest.spyOn(window, 'confirm').mockImplementation(() => {
+      return true;
+    });
+    jest.spyOn(axios, 'post').mockResolvedValue({});
+    const matchingRender = render(matching);
+    await waitFor(() => screen.getByText('매칭이 완료되었습니다'));
+    const endButton = matchingRender.container.querySelector('#endButton');
+    fireEvent.click(endButton!);
+    await waitFor(() => screen.getByText('아래의 매칭 버튼을 눌러 1대1 매칭을 시작하세요'));
   });
   xit('useInterval', () => {
     const matchingRender = render(matching);

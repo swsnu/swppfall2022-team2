1abac89dd842abe10625611d0c59f663f7217078
John-gh-kim <dygks63@snu.ac.kr>
2022-11-26 17:59:50 +0900

    chat

---
+++ frontend/src/store/slices/chat.test.ts
@@ -0,0 +1,14 @@
+import {  AnyAction, configureStore, EnhancedStore} from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+jest.mock('axios');
+
+describe(
+    "chat reducer", () => {
+
+    let store : 
+
+    });
+
+

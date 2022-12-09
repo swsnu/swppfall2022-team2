fd2d80e35ffe2c1de0b702c082ff8872e97fe655
byoungwookan97 <byoungwookan@snu.ac.kr>
2022-12-09 22:15:28 +0900

    fixes in backend test

--- backend/babchingu/menu/tests.py
+++ backend/babchingu/menu/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from mypage.models import UserInfo
+import json
+from .models import TodayMenu
+from rest_framework.authtoken.models import Token
 # Create your tests here.
+
+class ChatTestCase(TestCase):
+    def setUp(self):
+        #menus
+        meal1 = TodayMenu(mealtype="breakfast", menuplace="school", menuname="soup", menuprice="3500", menuextra="")
+        meal1.save()
+
+    def test_menu(self):
+        client = Client()
+        response = client.post('/menu/', json.dumps({}), content_type='application/json')
+        self.assertEqual(response.status_code, 405)
+        response = client.get('/menu/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), '[{"mealtype": "breakfast", "menuplace": "school", "menuname": "soup", "menuprice": "3500", "menuextra": ""}]')
